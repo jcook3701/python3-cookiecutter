7adba628d9c19b65d089490e00026b61bb41f433
jcook3701 <jcook3701@gmail.com>
2025-10-13 23:33:27 -0700

    tests-fixes

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -11,7 +11,7 @@ def test_bake_with_defaults(cookies):
 
 def test_bake_with_custom_name(cookies):
     """Ensure custom project_slug works."""
-    result = cookies.bake(extra_context={"project_slug": "custom_name"})
+    result = cookies.bake(extra_context={"package_name": "custom_name"})
     assert result.exit_code == 0
     assert result.exception is None
     assert result.project.basename == "custom_name"
